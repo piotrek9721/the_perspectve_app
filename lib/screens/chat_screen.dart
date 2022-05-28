2e5c0654a0d521f7a3cdb0d6502dcd115335d512
piotrekkruk <piotrekkruk@icloud.com>
2022-05-28 16:07:12 +0200

    0.0.0

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flash_chat/constants.dart';
+import 'package:the_perspectve_app/constants.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
